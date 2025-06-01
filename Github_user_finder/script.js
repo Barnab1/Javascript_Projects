bde5237dd4ad0d6819729c67a43f6b57bf07a359
AGUEH Barnabé Mahugnon <barnabeagueh@gmail.com>
2025-06-01 18:30:00 +0000

    Small improvements made with Gemini  AI

--- Github_user_finder/script.js
+++ Github_user_finder/script.js
@@ -23,20 +23,47 @@ fetch(`https://api.github.com/users/${username}`, { headers })
     <img class="profile" src="${data['avatar_url']}" alt="Github user profile">
     <div class="user-information">
        <p id="github_username">Github username:<strong>${data['login']}</strong></p>
-       <a href="${data['html_url']}">Find on Github</a> 
+       <a href="${data['html_url']}" target="_blank">Find on Github</a> 
     </div>
 </div>`;
   response.innerHTML= result;
 
 }
   )
-  .catch(error => response.innerHTML = `Not Found, Please try again`);
+  .catch(error => response.innerHTML = `Github User not Found, Please try again`);
 
+}
+const sanitizeInput = userInput =>{
+  return userInput.trim();
+}
+
+const checkEmptyInput = userInput =>{
+  if(userInput === ""){
+    alert("Provide Github user's name");
+    return;
+  }
+}
+
+const disableButton = ()=>{
+  searchBtn.disabled = true;
+  searchBtn.style.backgroundColor = 'whitesmoke';
+  searchBtn.style.color = "#000";
+}
+
+const enableButton = ()=>{
+  searchBtn.disabled = false;
+  searchBtn.style.backgroundColor = 'rgb(58, 184, 230)';
+  searchBtn.style.color = "#000";
 }
 
 searchBtn.addEventListener('click', (e)=>{
-  const username = githubUserName.value;
+  
+  disableButton();
   e.preventDefault();
+  const username = sanitizeInput(githubUserName.value);
+  checkEmptyInput(username);
+
   console.log(username);
   findUserInformation(username);
+  enableButton();
 })
